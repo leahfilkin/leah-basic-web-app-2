071d60e6159e6b84a95c8bbf2841c1e95b3f5263
Leah Filkin <leah.filkin@myob.com>
2021-10-14 16:46:59 +1300

    updating docker tag

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,2 +1,2 @@
-docker tag leah-basic-web-app-2 docker.myob.com/leah-basic-web-app-2/leah-basic-web-app-2
+docker tag leahfilkin/leah-basic-web-app-2 docker.myob.com/leah-basic-web-app-2/leah-basic-web-app-2
 docker push docker.myob.com/leah-basic-web-app/leah-basic-web-app-2
