7d8d7c2a13acd217e8052d8c48e53a607a71f8d7
Leah Filkin <leah.filkin@myob.com>
2021-10-19 16:10:47 +1300

    testing out image tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
-docker build -t docker.myob.com/future-makers-academy/leah-basic-web-app-2 .
-docker push docker.myob.com/future-makers-academy/leah-basic-web-app-2
+docker build -t docker.myob.com/future-makers-academy/leah-basic-web-app-2:$TAG .
+docker push docker.myob.com/future-makers-academy/leah-basic-web-app-2:$TAG
