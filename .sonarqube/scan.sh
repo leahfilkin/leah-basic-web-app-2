6d7980fa97c6687ea9114735e9dda4f39f1bc7d5
Leah Filkin <leah.filkin@myob.com>
2021-10-21 16:43:40 +1300

    trying to implement sonarqube

---
+++ .sonarqube/scan.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function die { echo "$*" 1>&2 ; exit 1; }
+function ssm-get {
+	var=$(aws ssm get-parameter --name "$1" --with-decryption --query Parameter.Value --output text 2>/dev/null)
+	[[ -z "$var" ]] && die "Error while retrieving $1"
+	echo "$var"
+}
+
+PROJECT_KEY="sonar-ops-and-arch:leah-basic-web-app-2"
+PROJECT_NAME="leah-basic-web-app-2"
+TOKEN=$(ssm-get "/etc/tokens/sonarqube")
+SOLUTION_FILE="BasicWebApp/BasicWebApp.sln"
+
+
+docker build \
+  -f .sonarqube/Dockerfile \
+  --build-arg PROJECT_KEY="$PROJECT_KEY" \
+  --build-arg PROJECT_NAME="$PROJECT_NAME" \
+  --build-arg TOKEN="$TOKEN" \
+  --build-arg SOLUTION_FILE="$SOLUTION_FILE" \
+  .
