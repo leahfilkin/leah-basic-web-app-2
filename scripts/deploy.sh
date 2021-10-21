c9a6be62f4bb83b9908538f69442637c9c5d4cf9
Leah Filkin <leah.filkin@myob.com>
2021-10-22 12:59:20 +1300

    attempting to add preprod and prod steps seperated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,13 @@
-kubectl apply -f ./jupiter/template.yml -n fma
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$#" -ne 2 ]]; then
+  echo "usage: deploy <env> <image_tag> (eg. where env: [preprod|prod])";
+  exit 1;
+fi
+
+# deploy...
+env="${1}"
+imageTag="${2}"
+
+ktmpl -f "jupiter/${env}.yml" -p imageTag "${imageTag}" jupiter/template.yml | kubectl apply -f -
