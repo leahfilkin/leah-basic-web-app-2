03b09ef12dca179871c7157d43e3bc730ba97353
Leah Filkin <leah.filkin@myob.com>
2021-10-22 13:17:29 +1300

    simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [[ "$#" -ne 2 ]]; then
-  echo "usage: deploy <env> <image_tag> (eg. where env: [preprod|prod])";
-  exit 1;
-fi
-
-# deploy...
-env="${1}"
-imageTag="${2}"
-
 ktmpl -f "jupiter/${env}.yml" -p imageTag "${imageTag}" jupiter/template.yml | kubectl apply -f -
