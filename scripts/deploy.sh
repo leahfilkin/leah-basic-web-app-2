c9fb20e5749dd94249e2d533bcd4dbb26b09e6ba
Leah Filkin <leah.filkin@myob.com>
2021-10-22 14:48:57 +1300

    trying to fix script?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
-set -euo pipefail
+#!/usr/bin/env bash
 
 if [[ "$#" -ne 2 ]]; then
-  echo "usage: deploy <env> <image_tag> (eg. where env: [preprod|prod])";
+  echo "usage: deploy <env> <image_tag>";
   exit 1;
 fi
 
 # deploy...
 env="${1}"
 imageTag="${2}"
+
+if [[ $env == "prod" ]]
+then
+    deployment="leah-filkin-app"
+elif [[ $env -eq "preprod" ]]
+then
+    deployment="leah-filkin-app-preprod"
+fi
 
 ktmpl -f "jupiter/${env}.yml" -p imageTag "${imageTag}" jupiter/template.yml | kubectl apply -f -
