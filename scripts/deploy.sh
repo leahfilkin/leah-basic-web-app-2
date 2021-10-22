d55c65dff4837cec499ed352fa1e4dc716a1b6a5
Leah Filkin <leah.filkin@myob.com>
2021-10-22 13:26:43 +1300

    change permissions deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ "$#" -ne 2 ]]; then
+  echo "usage: deploy <env> <image_tag> (eg. where env: [preprod|prod])";
+  exit 1;
+fi
+
+# deploy...
+env="${1}"
+imageTag="${2}"
+
 ktmpl -f "jupiter/${env}.yml" -p imageTag "${imageTag}" jupiter/template.yml | kubectl apply -f -
