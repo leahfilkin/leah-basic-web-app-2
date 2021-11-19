2941fa6443901333ec95a30c8fd70994467d976f
Leah Filkin <leah.filkin@myob.com>
2021-11-19 14:20:03 +1300

    changing startup back

--- BasicWebApp/BasicWebApp/Startup.cs
+++ BasicWebApp/BasicWebApp/Startup.cs
@@ -32,7 +32,7 @@ namespace BasicWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            if (Environment.GetEnvironmentVariable("AUTHENTICATION_TOKEN"))
+            if (HostEnvironment.IsProduction())
             {
                 services.AddDbContext<UsersContext>(options =>
                     options.UseNpgsql(Configuration.GetSection("Database").Get<DatabaseConfig>().ConnectionString));
