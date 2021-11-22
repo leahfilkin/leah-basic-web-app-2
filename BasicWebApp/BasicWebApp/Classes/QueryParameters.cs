4603d5046953b645262d63fdb829b43cb2eba5a1
Leah Filkin <leah.filkin@myob.com>
2021-11-22 15:24:25 +1300

    trying to get code smells out without breaking tests

--- BasicWebApp/BasicWebApp/Classes/QueryParameters.cs
+++ BasicWebApp/BasicWebApp/Classes/QueryParameters.cs
@@ -8,10 +8,19 @@ namespace BasicWebApp.Classes
         private readonly int _size = 10;
         private const int MaxPage = 100;
         private readonly int _page= 1;
+        private readonly string _sortBy = string.Empty;
+        private readonly string _order = string.Empty;
         
-        public string SortBy { get; init; } = string.Empty;
+        public string SortBy
+        {
+            get => _sortBy;
+            init => _sortBy = value;
+        }
 
-        public string Order { get; init; } = string.Empty;
+        public string Order {             
+            get => _order;
+            init => _order = value;
+        }
 
         public int Page {             
             get => _page;
